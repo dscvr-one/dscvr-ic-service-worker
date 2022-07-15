b7369d96a21191e2f42a5eb7eade86e79ed30652
Rick Porter <rick.e.porter@gmail.com>
2022-07-15 13:23:59 -0700

    logging for sw

--- src/sw/sw.ts
+++ src/sw/sw.ts
@@ -6,10 +6,12 @@ const DEBUG = true;
 
 // Always install updated SW immediately
 self.addEventListener('install', () => {
+  console.log("Install");
   self.skipWaiting();
 });
 
 self.addEventListener('activate', async () => {
+  console.log("activate");
   // upon activation take control of all clients (tabs & windows)
   await self.clients.claim();
   // reload all clients
@@ -19,10 +21,12 @@ self.addEventListener('activate', async () => {
 
 // Intercept and proxy all fetch requests made by the browser or DOM on this scope.
 self.addEventListener('fetch', (event: FetchEvent) => {
+  console.log("fetch");
   try {
     const response = handleRequest(event.request);
     event.respondWith(response);
   } catch (e) {
+    console.log('addEventListener exception', e)
     const error_message = String(e);
     console.error(error_message);
     if (DEBUG) {
